704c578c9c16e7b2e8f5676600fefae8b4ecbf11
Nick Postma <npostma@awareofart.nl>
2014-12-10 21:37:07 +0100

    Detectie van weerstanden icm de tolerantie

--- ResistorDetector.ino
+++ ResistorDetector.ino
@@ -1,15 +1,18 @@
 #include "resistortypes.h"
 
-int analogPin = 0;     // Pin waar de voltage verdeler uitlezen
+#define NUM_MEASURE_POINTS 2
 
 int raw = 0;           
-int Vin = 5;
-
+float Vin = 5.0;
 float Vout = 0;        
-float R1 = 100;        
 float R2 = 0;          
-float buffer = 0;      
+float buffer = 0;    
 
+float R_MEASURE[NUM_MEASURE_POINTS] = 
+{
+    100,  // A0
+    1000  // A1
+};
 
 void setup()
 {
@@ -19,32 +22,94 @@ void setup()
 
 void loop()
 {
-  raw = analogRead(analogPin);    
+    int detected_pin = -1;
+    
+    for(int i = 0; i < NUM_MEASURE_POINTS; i++) {
+      R2 = measure(R_MEASURE[i], i);
+      if(R2 != -1) {
+        detected_pin = i;
+        break;
+      }
+    }
+    
+    if(detected_pin == -1) {
+        Serial.println("Ongeldige meting. Probeer een ander berijk");
+        delay(200);
+        return;
+    }
+       
+    long R2_type;      
+    float current_tolerance;    
+    int num_found = 0;
+    for(int i = 0; i < NUM_TOLARANCE_TYPES; i++) {
+        int num = 0;
+        current_tolerance = tolerance_types[i];
+        R2_type = findResistor(current_tolerance, num);
+        if(num != 0) {
+            num_found = num;
+            break;
+        }
+    }
+    
+    if(num_found == 0) {
+      Serial.println("Weerstand kon niet bepaald worden");
+      return;
+    }
+
+    Serial.print("On pin:");           
+    Serial.print(detected_pin);         
+    Serial.println(":");      
+    
+    Serial.print("\tVoltage: ");      
+    Serial.println(Vout);      
+    
+    Serial.print("\tR2: ");           
+    Serial.print(R2);               
+    Serial.println(" Ohm");
+    
+    Serial.print("\tR2 type: ");            
+    Serial.print(" [");      
+    Serial.print(R2_type);  
+    Serial.print("]");      
+    Serial.println(" Ohm");
+    
+    Serial.print("\tTolerantie: ");           
+    Serial.print(current_tolerance);     
+    Serial.println("%");
+    
+    Serial.print("\tZekerheid: ");           
+    Serial.print(100 / num_found);     
+    Serial.println("%");
+    delay(1000);
+  
+}
+
+long findResistor(float tolerance, int &num_found) {
+  long R2_type_value = 0;
+    for(int i = 0; i < NUM_RESISTORS_TYPES; i++) {
+        long diff = resistor_types_tenth[i] - (R2 * 10);
+        
+        long th = ((float)resistor_types_tenth[i]  *  tolerance);
+            
+        // Type bepalen
+        if(diff > -th && diff < th) {
+            R2_type_value = resistor_types_tenth[i] / 10;
+            num_found++;
+        }
+    } 
+    return R2_type_value;
+}
+
+float measure(int r, int pin) 
+{
+  raw = analogRead(pin);    
   
   Vout = (5.0 / 1023.0) * raw;    // Bereken de voltage van de voltage verdeler
   
+  if(Vout == 5) {
+      return -1;
+  }
+  
   buffer = (Vin / Vout) - 1;
-  R2 = (R1 / buffer);
-  
-  int R2_type = 0;
-  for(int i = 0; i < 45; i++) {
-    int diff = resistor_types_tenth[i] - (R2 * 10);
-    
-    // Threashold is 10% van het type. Standaard gouden ring. TODO: variabel maken omdat er ook weerstanden zijn met preciecie van 1%, 2%, 5%, 20% 
-    int th = resistor_types_tenth[i]  * 0.1;
-    // Type bepalen
-    if(diff > -th && diff < th) {
-        R2_type = resistor_types_tenth[i] / 10;
-    }
-  } 
-  
-  Serial.print("Voltage: ");      //
-  Serial.println(Vout);           // Outputs the information
-  Serial.print("R2: ");           //
-  Serial.print(R2);               //
-  Serial.println("Ohm");
-  Serial.print("R2 type: ");      //
-  Serial.print(R2_type);          //
-  Serial.println("Ohm");
-  delay(1000);
+  return (r / buffer);
 }
